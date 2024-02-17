cfa1477034e1bc304a498e2246815ec56f6e95b5
Wellison Silva <wonsilva100@gmail.com>
2024-02-17 20:21:10 -0300

    added migrations

--- migrations/20240216161900_create_table_orders.js
+++ migrations/20240216161900_create_table_orders.js
@@ -3,7 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('orders', order => {
+        order.increments('id').primary()
+        order.string('client')
+        order.string('product').notNullable()
+        order.integer('quantity').notNullable()
+        order.string('product2')
+        order.integer('quantity2')
+        order.dateTime('orderTime').notNullable()
+        order.double('formPayment').notNullable
+
+    })
 };
 
 /**
